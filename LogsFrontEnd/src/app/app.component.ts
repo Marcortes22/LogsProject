a0bb99111bf87ee7f589caab471c904973c22308
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2024-10-11 16:03:45 -0600

    feat/ Se agrego el componente basico del login en los logs y se medio configuraron las rutas

--- LogsFrontEnd/src/app/app.component.ts
+++ LogsFrontEnd/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ErrorLogsComponent } from './components/error-logs/error-logs.component';
+import { LoginComponent } from "./components/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ErrorLogsComponent],
+  imports: [RouterOutlet, ErrorLogsComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- LogsFrontEnd/src/app/app.routes.ts
+++ LogsFrontEnd/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorLogsComponent } from './components/error-logs/error-logs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'error-logs', component: ErrorLogsComponent }
+];
