1b47cf7436044b05eede26501fdf2bc3e947c3c3
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2024-10-12 16:17:53 -0600

    feat/ Estilos actualizados para el login, por mientras para tener algo que no se vea feo con posibles cambios

--- LogsFrontEnd/src/app/components/login/login.component.ts
+++ LogsFrontEnd/src/app/components/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -15,15 +16,24 @@ import { Router } from '@angular/router';
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    FormsModule
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
+
 })
 export class LoginComponent {
+  hide = true;
+
   constructor(private router: Router) {}
 
-  onLogin() {
+  togglePasswordVisibility(event: Event) {
+    event.preventDefault();
+    this.hide = !this.hide;
+  }
 
+  onLogin() {
+ 
     this.router.navigate(['/error-logs']);
   }
 }
