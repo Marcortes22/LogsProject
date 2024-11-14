0ddf6db207708e3651df4be7369deb349643c1ed
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2024-11-14 17:35:55 -0600

    Feat/ Rutas

--- LogsFrontEnd/src/app/app.routes.ts
+++ LogsFrontEnd/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ErrorLogsComponent } from './components/error-logs/error-logs.component';
 import { LayoutComponent } from './components/layout/layout.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
@@ -9,7 +10,8 @@ export const routes: Routes = [
     path: '',
     component: LayoutComponent,
     children: [
-      { path: 'error-logs', component: ErrorLogsComponent }
+      { path: 'error-logs', component: ErrorLogsComponent, canActivate: [AuthGuard] }
     ]
-  }
+  },
+  { path: 'forbidden', component: ForbiddenComponent }
 ];
---
+++ LogsFrontEnd/src/app/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decodedToken = this.authService.decodeToken(token);
+      if (decodedToken && decodedToken.role === 'LogError') {
+        return true;
+      }
+    }
+    this.router.navigate(['/forbidden']);
+    return false;
+  }
+}
